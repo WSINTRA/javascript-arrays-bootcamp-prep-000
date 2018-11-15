e9800b074da7bb9805787b1396722dc0c90d9e03
William Robert Simpson <will.simpson85@gmail.com>
2018-11-15 18:40:09 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -20,7 +20,10 @@ function destructivelyAddElementToEndOfArray(array,element){
 function accessElementInArray(array,index){
   return array[index];
 }
-function destructivelyRemoveElementFromBeginningOfArray(array){}
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift()
+  return array
+}
 function removeElementFromBeginningOfArray(array){}
 function destructivelyRemoveElementFromEndOfArray(array){}
 function removeElementFromEndOfArray(array){}
