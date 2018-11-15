aa07380da5e3f866f150299fcb0323cb294963eb
William Robert Simpson <will.simpson85@gmail.com>
2018-11-15 18:44:24 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -28,5 +28,9 @@ function removeElementFromBeginningOfArray(array){
   return array.slice(1)
 
 }
-function destructivelyRemoveElementFromEndOfArray(array){}
-function removeElementFromEndOfArray(array){}
+function destructivelyRemoveElementFromEndOfArray(array){
+  return array.pop()
+}
+function removeElementFromEndOfArray(array){
+  return array.slice(0, array.length - 1)
+}
